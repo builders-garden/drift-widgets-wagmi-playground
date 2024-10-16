e065289b453922746ad278e2a013ad4b50a61c88
lim0n3 <simonestaffa96@gmail.com>
2024-10-16 09:40:26 +0200

    upgrade to 0.0.11

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base, baseSepolia } from "viem/chains";
+import { base, mainnet, optimism, polygon, arbitrum } from "viem/chains";
 
 export const config = getDefaultConfig({
   appName: "Drift Widget Playground",
   projectId: "YOUR_PROJECT_ID",
-  chains: [base, baseSepolia],
+  chains: [base, mainnet, optimism, polygon, arbitrum],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
